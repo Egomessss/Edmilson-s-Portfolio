d2c28db19ce85c13a9fdf300a197fe5d6e2d75a6
Egomessss <ed.gomes97@gmail.com>
2023-01-14 20:12:36 +0000

    framer motion v1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { motion } from "framer-motion"
 import { Cursor, useTypewriter } from "react-simple-typewriter"
+import BackGroundCircles from "./BackGroundCircles"
 
 function Hero() {
   const [text, count] = useTypewriter({
@@ -10,22 +11,51 @@ function Hero() {
   })
   return (
     <div>
-      <motion.div>
-        <img
-          src=""
-          alt=""
-        />
-        <h1>Hi I'm Edmilson Gomes</h1>
+ <motion.div
+    initial={{
+        x:500,
+        opacity:0,
+        scale:0.5,
+    }}
+    animate={{
+        x:0,
+        opacity:1,
+        scale:1,
+    }}
+    transition={{duration:1}}>
 
-        <h2>
-          <span>
-            {text}
-            <Cursor cursorColor="white" />
-          </span>
-        </h2>
-      </motion.div>
+    </motion.div>
+    
+    <motion.div
+initial={{
+    x:500,
+    opacity:0,
+    scale:0.5,
+}}
+animate={{
+    x:0,
+    opacity:1,
+    scale:1,
+}}
+transition={{duration:1}}>
+    {/* adicionar text a dizer"this is the story of a developer who could center a div in his first try" no inicio depois falho a center div , depois mete no centro, e digo "ok first try is impossible, but he gets the job done" isto em h1 acima da animacao do hero e depois dá fade out*/}
+    <div className="h-screen flex flex-col space-y-12 items-center justify-center text-center overflow-hidden">
+      <BackGroundCircles />
+      <img
+        src=""
+        alt=""
+      />
+      <h1>Hi I'm Edmilson Gomes</h1>
+      <h2>
+        <span>{text}</span>
+        <Cursor cursorColor="white" />
+      </h2>
     </div>
+    </motion.div>
+    </div>
+   
   )
 }
 
 export default Hero
+ 
