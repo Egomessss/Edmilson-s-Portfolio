dfe737fb71e1faa9b6c0f085fe5fb7aae917e6aa
Egomessss <ed.gomes97@gmail.com>
2023-01-14 15:43:49 +0000

    added react icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,17 @@ import React from "react"
 
 function Navbar() {
   return (
-    <nav>
-      <h1 className="font-aquirebold text-4xl">hello</h1>
-    </nav>
+  <nav>
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4">
+        <h1 className="w-full text-3xl font-aquirebold">Home</h1>
+        <ul className="flex whitespace-nowrap">
+            <li className="p-4">About</li>
+            <li className="p-4">Why You Should Hire Me</li>
+            <li className="p-4">Projects</li>
+            <li className="p-4">Contact Me</li>
+        </ul>
+    </div>
+  </nav>
   )
 }
 
