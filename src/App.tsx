52c1b4d9a95e0fbfb684ecb0c2915e341c401886
Egomessss <ed.gomes97@gmail.com>
2023-01-15 16:38:46 +0000

    added experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import About from "./components/About"
+import Experience from "./components/Experience"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Skills from "./components/Skills"
 
 function App() {
-  
   return (
-   <div className="max-w-[1240px] mx-auto px-4">
-   <Navbar/>
-   <Hero/>
-   <About/>
-   <Skills/>
-   </div>
+    <div className="max-w-[1240px] mx-auto px-4">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Experience />
+    </div>
   )
 }
 
---
+++ src/components/Experience.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { TbError404 } from "react-icons/tb"
+
+function Experience() {
+  return (
+    <div className="h-screen">
+      <h2 className="text-3xl">Experience</h2>
+      <div className="flex flex-col justify-center items-center mt-52">
+        <TbError404 className="text-9xl" />
+        <p className="text-3xl text-center">
+          Uncaught typeError : cannot set property “innerHTML” of null at
+          &lt;workExperience&gt; Hire me to fix this error in the future.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Experience
