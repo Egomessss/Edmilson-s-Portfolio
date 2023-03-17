74a635942d9efe6eb4761fb0fc26c495daa59c61
Egomessss <111806970+Egomessss@users.noreply.github.com>
2023-03-17 23:10:55 +0000

    contacts - added logos and text sizes changes

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,7 +1,5 @@
-import React from "react"
-import Footer from "./Footer"
-// !para validar form
-// https://tailwindcomponents.com/component/floating-label-contact-form
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai"
+import { SiGmail } from "react-icons/si"
 
 function Contacts() {
   return (
@@ -10,16 +8,18 @@ function Contacts() {
       <div className="flex flex-col gap-5">
         <button
           onClick={() => navigator.clipboard.writeText("ed.gomes97@gmail.com")}
-          className="bg-gradient-to-r from-[#4285F4] via-[#D7322A]  to-[#34A853] text-start ">
-            {/* change the text size */}
-          <h1 className="text-white text-xl">Gmail - Copy to clipboard</h1>
+          className="bg-gradient-to-r from-[#4285F4] via-[#D7322A]  to-[#34A853] text-start  flex items-center gap-2">
+          {/* change the text size */}
+          <SiGmail  className="text-7xl border-r-2 px-1"/>
+          <h1 className="text-white md:text-7xl">Gmail - Copy to clipboard</h1>
         </button>
-        {/* bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 */}
-        <button className="bg-[#0A66C2] text-start">
-          <h1 className="text-white">Linkedin</h1>
+        <button className="bg-[#0A66C2] text-start flex items-center gap-2">
+          <AiFillLinkedin className="text-7xl border-r-2 px-1" />
+          <h1 className="text-white md:text-7xl">Linkedin</h1>
         </button>
-        <button className="bg-[#161B22] text-start">
-          <h1 className="text-white">Github</h1>
+        <button className="bg-[#161B22] text-start flex items-center gap-2">
+          <AiFillGithub className="text-7xl border-r-2 px-1" />
+          <h1 className="text-white md:text-7xl">Github</h1>
         </button>
       </div>
     </div>
