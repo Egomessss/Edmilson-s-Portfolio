df55e918dcd36cfe4b8d621f48a3eaa41d3473b3
Egomessss <111806970+Egomessss@users.noreply.github.com>
2023-03-18 20:33:23 +0000

    projects fixed

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -122,102 +122,113 @@ function Projects() {
         </div>
       </div>
       {/* mobile */}
-      <div className="lg:hidden">
+      <div className="flex flex-col gap-4 xl:hidden">
         {/* firebnb */}
-        <img
-          className="col-span-2"
-          src={firebnb}
-          alt="airbnb clone"
-        />
-        <div className="col-span-2 flex flex-col items-end justify-center gap-2 border-2 border-white">
-          <h3 className="text-right">Airbnb Clone</h3>
-          <p className="text-right text-neongreen">
-            Javascript | React | Tailwind | Mapbox Api
-          </p>
-          <p className=" text-right">
-            Airbnb clone with all the main navigation functionalities, which i
-            did so i could learn a bit of UX and UI design before so i could get
-            some inspiration for my future apps
-          </p>
-          <div className="flex justify-center gap-6 p-4 align-middle">
-            <a
-              href="#_"
-              className="group relative px-4  py-2 text-black">
-              <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
-              <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
-              <span className="relative font-aquirebold">Live Demo</span>
-            </a>
-            <a
-              href="#_"
-              className="group  relative px-4 py-2 font-bold text-black">
-              <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
-              <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
-              <span className="relative font-aquirebold">Code</span>
-            </a>
+        <div className="max-w-100% group relative flex max-h-[450px] w-full">
+          <img
+            className="pointer"
+            src="src\assets\images\firebnb.png"
+            alt=""
+          />
+          {/* Hover effect */}
+          <div className=" absolute inset-x-0 bottom-0 flex h-full max-w-sm flex-col items-start justify-center gap-2 border-2 border-white bg-[#101010]/80 px-2 duration-300 group-hover:opacity-100">
+            {" "}
+            <h3 className=" tracking-tight">Amazon E-commerce Store Clone</h3>
+            <p className=" text-neongreen">
+              Typescript | React | Tailwind | Api{" "}
+            </p>
+            <p>
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <div className="flex justify-center gap-6 p-4 align-middle">
+              <a
+                href="#_"
+                className="group relative px-4  py-2 text-black">
+                <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
+                <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
+                <span className="relative font-aquirebold">Live Demo</span>
+              </a>
+              <a
+                href="#_"
+                className="group  relative px-4 py-2 font-bold text-black">
+                <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
+                <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
+                <span className="relative font-aquirebold">Code</span>
+              </a>
+            </div>
           </div>
         </div>
         {/* portfolio */}
-        <div className=" col-span-2 flex  flex-col items-center justify-center gap-2 border-2 border-white ">
-          <h3>My Personal Portfolio</h3>
-          <p className="m-2 text-neongreen">Javascript | React | Tailwind</p>
-          <p className=" text-center">
-            A display of my creativity and simplicity
-          </p>
-          <div className="flex justify-center gap-6 p-4 align-middle">
-            <a
-              href="#_"
-              className="group relative px-4  py-2 text-black">
-              <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
-              <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
-              <span className="relative font-aquirebold">Live Demo</span>
-            </a>
-            <a
-              href="#_"
-              className="group  relative px-4 py-2 font-bold text-black">
-              <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
-              <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
-              <span className="relative font-aquirebold">Code</span>
-            </a>
+        <div className="max-w-100% group relative flex max-h-[450px] w-full">
+          <img
+            className="pointer"
+            src="src\assets\images\firebnb.png"
+            alt=""
+          />
+          {/* Hover effect */}
+          <div className=" absolute inset-x-0 bottom-0 flex h-full max-w-sm flex-col items-start justify-center gap-2 border-2 border-white bg-[#101010]/80 px-2 duration-300 group-hover:opacity-100">
+            {" "}
+            <h3 className=" tracking-tight">Amazon E-commerce Store Clone</h3>
+            <p className=" text-neongreen">
+              Typescript | React | Tailwind | Api{" "}
+            </p>
+            <p>
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <div className="flex justify-center gap-6 p-4 align-middle">
+              <a
+                href="#_"
+                className="group relative px-4  py-2 text-black">
+                <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
+                <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
+                <span className="relative font-aquirebold">Live Demo</span>
+              </a>
+              <a
+                href="#_"
+                className="group  relative px-4 py-2 font-bold text-black">
+                <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
+                <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
+                <span className="relative font-aquirebold">Code</span>
+              </a>
+            </div>
           </div>
         </div>
-        <img
-          className="col-span-2"
-          src={firebnb}
-          alt="Personal portfolio"
-        />
         {/* u-Mov */}
-        <img
-          className="col-span-2"
-          src={firebnb}
-          alt="airbnb clone"
-        />
-        <div className=" col-span-2 flex flex-col items-start justify-center gap-2 border-2 border-white  ">
-          <h3 className="text-left">u-Mov</h3>
-          <p className="text-left text-neongreen">
-            Typescript | Nextjs | Tailwind | Google API
-          </p>
-          <p className=" text-left">
-            Urban movement made easy - everything someone needs when moving in
-            one place.
-            <span className="font-semibold">
-              Disclaimer: In-Development, to be shown in interviews.
-            </span>
-          </p>
-          <div className="flex justify-center gap-6 p-4 align-middle">
-            <a
-              href="#_"
-              className="group relative px-4  py-2 text-black">
-              <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
-              <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
-              <span className="relative font-aquirebold">Live Demo</span>
-            </a>
-            <a
-              href="#_"
-              className="group  relative px-4 py-2 font-bold text-black">
-              <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
-              <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
-              <span className="relative font-aquirebold">Code</span>
-            </a>
+        <div className="max-w-100% group relative flex max-h-[450px] w-full">
+          <img
+            className="pointer"
+            src="src\assets\images\firebnb.png"
+            alt=""
+          />
+          {/* Hover effect */}
+          <div className=" absolute inset-x-0 bottom-0 flex h-full max-w-sm flex-col items-start justify-center gap-2 border-2 border-white bg-[#101010]/80 px-2 duration-300 group-hover:bg-[#101010]/80">
+            {" "}
+            <h3 className=" tracking-tight">Amazon E-commerce Store Clone</h3>
+            <p className=" text-neongreen">
+              Typescript | React | Tailwind | Api{" "}
+            </p>
+            <p>
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <div className="flex justify-center gap-6 p-4 align-middle">
+              <a
+                href="#_"
+                className="group relative px-4  py-2 text-black">
+                <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
+                <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
+                <span className="relative font-aquirebold">Live Demo</span>
+              </a>
+              <a
+                href="#_"
+                className="group  relative px-4 py-2 font-bold text-black">
+                <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-neongreen transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
+                <span className="absolute inset-0 h-full w-full border-2 border-white"></span>
+                <span className="relative font-aquirebold">Code</span>
+              </a>
+            </div>
           </div>
         </div>
       </div>
